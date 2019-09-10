4c70a3c474bc8d07a73e42e8801566c8771554a8
Tripti Tayal <tripti.tayal@tavant.com>
2019-09-10 11:17:02 +0530

    Add

---
+++ Jenekins/src/main/java/test/Jenekins/App.java
@@ -0,0 +1,13 @@
+package test.Jenekins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
